4c164409c0fd5ea0efc697fefd5dc7a64a87da1b
Sam Vasta <samuel.m.vasta@gmail.com>
2018-06-20 19:18:34 -0500

    Refactored DebugGenerator to be independent of generator pack modules

---
+++ imageGenerator/generator-pack-1/src/test/java/com/samvasta/generatorpack1/it/DebugGenerator.java
@@ -0,0 +1,23 @@
+package com.samvasta.generatorpack1.it;
+
+import com.samvasta.common.interfaces.IGenerator;
+import com.samvasta.debuggenerator.GeneratorWindow;
+import com.samvasta.generatorpack1.samplegenerator.SimpleGenerator;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DebugGenerator
+{
+
+    public static void main(String...args){
+        IGenerator generator = new SimpleGenerator();
+
+        GeneratorWindow window = new GeneratorWindow(generator, new Dimension(1920, 1080));
+
+        window.setSize(1600, 900);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
